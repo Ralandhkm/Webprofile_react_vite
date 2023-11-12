e27c88056af4ec15cfc92122fa2f90a126c724ed
Ralandhkm <ralandhakim07@gmail.com>
2023-11-13 06:58:16 +0700

    edit dikit2

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import './dist/css/main.css';
+import './main.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
